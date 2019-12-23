b903607a0e5abd995557c88e633d83ee756630dc
Netkas <45276434+Netkas@users.noreply.github.com>
2019-12-23 14:25:46 -0300

    Added the req and configuration data for acm

--- src/IntellivoidAPI/IntellivoidAPI.php
+++ src/IntellivoidAPI/IntellivoidAPI.php
@@ -4,9 +4,48 @@
     namespace IntellivoidAPI;
 
 
+    use acm\acm;
+    use Exception;
+
+    $LocalDirectory = __DIR__ . DIRECTORY_SEPARATOR;
+
+    if(class_exists('acm\acm') == false)
+    {
+        include_once(__DIR__ . DIRECTORY_SEPARATOR . 'acm' . DIRECTORY_SEPARATOR . 'acm.php');
+    }
+
+    /**
+     * Class IntellivoidAPI
+     * @package IntellivoidAPI
+     */
     class IntellivoidAPI
     {
+        /**
+         * @var acm
+         */
+        private $acm;
+
+        /**
+         * @var mixed
+         */
+        private $DatabaseConfiguration;
+
+        /**
+         * IntellivoidAPI constructor.
+         */
         public function __construct()
         {
+            $this->acm = new acm(__DIR__, 'Intellivoid Accounts');
+
+            try
+            {
+                $this->DatabaseConfiguration = $this->acm->getConfiguration('Database');
+            }
+            catch (Exception $e)
+            {
+                print("There was an error while trying to parse the ACM configuration");
+                print($e->getMessage());
+                exit(0);
+            }
         }
     }
