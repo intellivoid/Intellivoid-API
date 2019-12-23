f8fe8fec58faca1819647449b9214e6bbbb3800d
Netkas <45276434+Netkas@users.noreply.github.com>
2019-12-23 14:39:22 -0300

    Added constructor to AccessKeyManager in \IntellivoidAPI\Managers

--- src/IntellivoidAPI/Managers/AccessKeyManager.php
+++ src/IntellivoidAPI/Managers/AccessKeyManager.php
@@ -4,7 +4,25 @@
     namespace IntellivoidAPI\Managers;
 
 
+    use IntellivoidAPI\IntellivoidAPI;
+
+    /**
+     * Class AccessKeyManager
+     * @package IntellivoidAPI\Managers
+     */
     class AccessKeyManager
     {
+        /**
+         * @var IntellivoidAPI
+         */
+        private $intellivoidAPI;
 
+        /**
+         * AccessKeyManager constructor.
+         * @param IntellivoidAPI $intellivoidAPI
+         */
+        public function __construct(IntellivoidAPI $intellivoidAPI)
+        {
+            $this->intellivoidAPI = $intellivoidAPI;
+        }
     }
