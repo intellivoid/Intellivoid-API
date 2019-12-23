21224380a0b761433ad030393ebe201bc994c475
Netkas <45276434+Netkas@users.noreply.github.com>
2019-12-23 14:34:02 -0300

    Added test for constructor

---
+++ tests/construct_test.php
@@ -0,0 +1,8 @@
+<?php
+    /** @noinspection PhpFullyQualifiedNameUsageInspection */
+
+    // Import Library
+    $Source = __DIR__ . DIRECTORY_SEPARATOR . '..' .DIRECTORY_SEPARATOR . 'src';
+    include_once($Source . DIRECTORY_SEPARATOR . 'IntellivoidAPI' . DIRECTORY_SEPARATOR . 'IntellivoidAPI.php');
+
+    $IntellivoidAPI = new \IntellivoidAPI\IntellivoidAPI();
