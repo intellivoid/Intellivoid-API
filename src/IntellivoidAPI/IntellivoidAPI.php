738e0c30fcdd6ccada63037fcac78ffa834c0112
Netkas <45276434+Netkas@users.noreply.github.com>
2019-12-24 14:56:27 -0300

    Added dep for msqg

--- src/IntellivoidAPI/IntellivoidAPI.php
+++ src/IntellivoidAPI/IntellivoidAPI.php
@@ -25,6 +25,11 @@
         include_once(__DIR__ . DIRECTORY_SEPARATOR . 'acm' . DIRECTORY_SEPARATOR . 'acm.php');
     }
 
+    if(class_exists('msqg\msqg') == false)
+    {
+        include_once(__DIR__ . DIRECTORY_SEPARATOR . 'msqg' . DIRECTORY_SEPARATOR . 'msqg.php');
+    }
+
     /**
      * Class IntellivoidAPI
      * @package IntellivoidAPI
