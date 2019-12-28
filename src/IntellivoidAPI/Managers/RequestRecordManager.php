49a05c12945d7301f4b45bb19a62e71389ba5e65
Netkas <45276434+Netkas@users.noreply.github.com>
2019-12-28 14:18:15 -0300

    Added method logRecord() to \IntellivoidAPI\Managers > RequestRecordManager

--- src/IntellivoidAPI/Managers/RequestRecordManager.php
+++ src/IntellivoidAPI/Managers/RequestRecordManager.php
@@ -4,10 +4,14 @@
     namespace IntellivoidAPI\Managers;
 
 
+    use IntellivoidAPI\Abstracts\SearchMethods\AccessRecordSearchMethod;
+    use IntellivoidAPI\Exceptions\DatabaseException;
     use IntellivoidAPI\IntellivoidAPI;
     use IntellivoidAPI\Objects\RequestRecord;
     use IntellivoidAPI\Objects\RequestRecordEntry;
+    use IntellivoidAPI\Utilities\Hashing;
     use IntellivoidAPI\Utilities\Validate;
+    use msqg\QueryBuilder;
     use ZiProto\ZiProto;
 
     /**
@@ -30,6 +34,13 @@
             $this->intellivoidAPI = $intellivoidAPI;
         }
 
+        /**
+         * Logs a request record into the database
+         *
+         * @param RequestRecordEntry $requestRecordEntry
+         * @return bool
+         * @throws DatabaseException
+         */
         public function logRecord(RequestRecordEntry $requestRecordEntry): bool
         {
             $access_record_id = 0;
@@ -147,7 +158,49 @@
                 $response_time = (float)$requestRecordEntry->ResponseTime;
             }
 
+            $timestamp = (int)time();
+            $reference_id = Hashing::calculateReferenceId(
+                $timestamp,
+                $application_id,
+                $access_record_id,
+                $ip_address,
+                $user_agent,
+                $path
+            );
+            $reference_id = $this->intellivoidAPI->getDatabase()->real_escape_string($reference_id);
+            $day = (int)date('d');
+            $month = (int)date('m');
+            $year = (int)date('Y');
 
+            $Query = QueryBuilder::insert_into('request_records', array(
+                'reference_id' => $reference_id,
+                'access_record_id' => $access_record_id,
+                'application_id' => $application_id,
+                'request_method' => $request_method,
+                'version' => $version,
+                'path' => $path,
+                'request_payload' => $request_payload,
+                'ip_address' => $ip_address,
+                'user_agent' => $user_agent,
+                'response_code' => $response_code,
+                'response_content_type' => $response_content_type,
+                'response_length' => $response_length,
+                'response_time' => $response_time,
+                'day' => $day,
+                'month' => $month,
+                'year' => $year,
+                'timestamp' => $timestamp
+            ));
+            $QueryResults = $this->intellivoidAPI->getDatabase()->query($Query);
 
+            if($QueryResults == true)
+            {
+                return true;
+            }
+            else
+            {
+                throw new DatabaseException($Query, $this->intellivoidAPI->getDatabase()->error);
+            }
         }
+
     }
